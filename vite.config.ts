51cc71000ad5642b7c11f77e0b5ec4562cf434d9
zydan25 <zydanalattab25w@gmail.com>
2026-09-13 17:09:26 +0300

    Make Vite build source and GitHub Pages compatible

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,8 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/flutter8/' : '/',
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    strictPort: true,
-    allowedHosts: true
-  },
-  preview: {
-    port: 3000,
-    host: '0.0.0.0',
-    strictPort: true,
-    allowedHosts: true
-  }
+  server: { port: 3000, host: '0.0.0.0', strictPort: true, allowedHosts: true },
+  preview: { port: 3000, host: '0.0.0.0', strictPort: true, allowedHosts: true },
 });
